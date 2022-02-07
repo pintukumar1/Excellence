7ff1be5cda09fe1fbb6a9186924844bcc33e5cfe
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-02-07 18:10:07 +0530

    first commit

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
 
-const layout = (props) => (
-    <Aux>
-        <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main className={classes.Content} >
-            {props.children}
-        </main>
-    </Aux>  
-);
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
 
-export default layout
+    render () {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} 
+                            closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content} >
+                    {this.props.children}
+                </main>
+            </Aux>  
+            )
+        }
+    }
+
+export default Layout
