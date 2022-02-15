ada37fb89fbb2579e62e4023e0c83d96bf7e2f27
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-02-15 09:51:10 +0530

    adding spinner

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../hoc/axios-orders'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -76,7 +77,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue!')
+        // alert('You Continue!')
+        const order = {
+            ingredients : this.state.ingredients,
+            price: this.state.totalPrice,
+            customer : {
+                name: 'Pintu Kumar',
+                address : {
+                    street: 'Tikhampur',
+                    ZipCode: '277001',
+                    Country: 'India'
+                },
+                email: 'kumarpintu1713122@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
     }
 
     render() {
